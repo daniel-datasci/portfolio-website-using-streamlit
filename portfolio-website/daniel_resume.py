56a455bfc0d970c8a7a77138a19116c64fc3ca2c
Daniel Ifediba <danifedibah@gmail.com>
2022-10-26 16:58:48 +0200

    Updated with requirements.txt file

--- portfolio-website/daniel_resume.py
+++ portfolio-website/daniel_resume.py
@@ -7,19 +7,23 @@ with open("style.css") as f:
 #####################
 # Header 
 st.write('''
-# Daniel Ifediba, Ph.D.
+# Daniel Ifediba
 ##### *Resume* 
 ''')
 
-image = Image.open('my_image.JPEG')
+image = Image.open('my_image.jpg')
 st.image(image, width=150)
+image1 = Image.open('sales.jpg')
+image2 = Image.open('house.jpg')
+image3 = Image.open('retire.jpg')
 
 st.markdown('## Summary', unsafe_allow_html=True)
 st.info('''
-- Data Scientist/Data Analyst
+- Data Scientist/Data Analyst.
 - Strong verbal and written communication skills.
 - Experienced storyteller using data and visualization tools.
 - Strong Knowledge of applying Machine Learning in real life scenarios and solving real time problems.
+-  Passionate about data and helping others kick start their data careers.
 ''')
 
 #####################
@@ -29,7 +33,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Chanin Nantasenamat</a>
+  <a class="navbar-brand" href="https://github.com/daniel-datasci" target="_blank">Daniel Ifediba</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -45,7 +49,7 @@ st.markdown("""
         <a class="nav-link" href="#work-experience">Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+        <a class="nav-link" href="#projects">Projects</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
@@ -86,7 +90,7 @@ def txt4(a, b, c):
     st.markdown(b)
   with col3:
     st.markdown(c)
-
+st.write('=======================================================================================')
 #####################
 st.markdown('''
 ## Education
@@ -94,12 +98,6 @@ st.markdown('''
 
 txt('**Data Science Accelerator Program**, *ExploreAI Academy*, South Africa',
 '2022')
-st.markdown('''
-- GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
-- Thesis awarded `1st` Prize by the National Research Council of Thailand.
-''')
 
 txt('**Data Science With Python**, *Simpli Learn*',
 '2022')
@@ -109,7 +107,7 @@ txt('**Data Science**, *Data Camp*',
 
 txt('**Bachelors of Science** (Accounting), *Anambra State Univrsity*, Nigeria',
 '2016')
-
+st.write('=======================================================================================')
 
 #####################
 st.markdown('''
@@ -138,45 +136,46 @@ st.markdown('''
 - Advised canceling customers on new procedures, persuading them to stay with the company resulting in a 30% decrease in cancellation.
 - Developed new methodologies in soliciting sales; increased sales and revenue turnover by 30% and 47% respectively
 ''')
-
+st.write('=======================================================================================')
 #####################
 st.markdown('''
-## Bioinformatics Tools
+## Projects
 ''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
-txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
-txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
-txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
-txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
-txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
-txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
-txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
-txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
-txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
-txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
-txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
-txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
-
-
+txt4('Retirement Income Predictor', 'Affiliated With ExploreAI', '[Project Source Code](https://github.com/daniel-datasci/Retirement-Income-Predictor)')
+st.image(image3)
+st.markdown('''
+Most people have a certain level of uncertainty nearing retirement age, as to if they will be able to reach their retirement goals. This uncertainty comes from factors such as; savings, health, number of dependents and so on. This project aims to reduce this levels of uncertainty and give people a higher level of confidence towards retirement by helping them know what income they should amass every month to reach this goal. The type of investment choice to be chosen is also adviced.
+''')
+st.write('---------------------------------------------------------------------------------------------------------------------------------------------')
+txt4('House Prices Prediction', 'Outsourced Project', '[Project Source Code](https://github.com/daniel-datasci/House-Prices-Prediction-1)')
+st.image(image2)
+st.markdown('''
+Ask a home buyer to describe their dream house, and they probably won't begin with the height of the basement ceiling or the proximity to an east-west railroad. But this playground competition's dataset proves that much more influences price negotiations than the number of bedrooms or a white-picket fence. This project aims to give people looking to seel or buy a house, the important factors that influece the hike or decline in price of houses.
+''')
+st.write('---------------------------------------------------------------------------------------------------------------------------------------------')
+txt4('Sales Revenue Insights', 'Outsourced Project', '[Project Source Code](https://github.com/daniel-datasci/Sales-Insights)')
+st.image(image1)
+st.markdown('''
+There are many times companies or simple business run into the difficulty of choosing which products they should focus on when they deal on different categories of products. This project aimed to give the said company clarity on how these various pproduct categories perform in sales over a period of 4 years. There is also a proffessional recommendation on which product should more resources be channeled to and which to be discarded.
+''')
+st.write('---------------------------------------------------------------------------------------------------------------------------------------------')
+txt4('Other Projects', '---','[Projects Source Code](https://github.com/daniel-datasci/)')
+st.write('=======================================================================================')
 #####################
 st.markdown('''
 ## Skills
 ''')
-txt3('Programming', '`Python`, `R`, `Linux`')
+txt3('Programming', '`Python`')
 txt3('Data processing/wrangling', '`SQL`, `Pandas`, `Numpy`, `Excel`')
-txt3('Data visualization', '`Matplotlib`, `Seaborn`, `Plotly`, `Powerbi`, `Excel`')
+txt3('Data visualization', '`Matplotlib`, `Seaborn`, `Powerbi`, `Excel`')
 txt3('Machine Learning', '`Scikit-learn`')
-txt3('Deep Learning', '`TensorFlow`')
 txt3('Model deployment', '`Streamlit`, `Heroku`, `AWS`')
-
+st.write('=======================================================================================')
 #####################
 st.markdown('''
 ## Social Media
 ''')
-txt2('LinkedIn', 'https://www.linkedin.com/in/daniel-ifediba-a681a4226/')
-txt2('Twitter', 'https://twitter.com/Daniel_Ifediba')
-txt2('GitHub', 'https://github.com/daniel-datasci/')
+txt2('LinkedIn', '[Connect With Me](https://www.linkedin.com/in/daniel-ifediba-a681a4226/)')
+txt2('Twitter', '[My Twitter Universe](https://twitter.com/Daniel_Ifediba)')
+txt2('GitHub', '[Dive Into To My Code Archive](https://github.com/daniel-datasci/)')
+txt2('Email Address', 'danifedibah@gmail.com')
